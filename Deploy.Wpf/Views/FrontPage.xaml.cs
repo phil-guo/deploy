be7d9f0bb88709979da5b067f8a26fa4c008ec47
phil <SkipperGuo@outlook.com>
2021-01-29 14:11:13 +0800

    添加front 默认dockerfile跟nginx.conf 配置

--- Deploy.Wpf/Views/FrontPage.xaml.cs
+++ Deploy.Wpf/Views/FrontPage.xaml.cs
@@ -1,11 +1,18 @@
 using System;
+using System.IO;
 using System.Windows;
+using Autofac;
 using Deploy.Appliction.Config;
+using Deploy.Appliction.Internal;
+using Microsoft.Extensions.Logging;
 
 namespace Deploy.Wpf.Views
 {
     public partial class FrontPage
     {
+        // private readonly ISftp _sftp;
+        private readonly ISsh _ssh;
+
         public FrontPage()
         {
             InitializeComponent();
@@ -25,8 +32,32 @@ namespace Deploy.Wpf.Views
 
         private void Deploy_Click(object sender, RoutedEventArgs e)
         {
-            Display.SelectedText = $"你好{Environment.NewLine}";
-            Display.SelectedText += $"不，我不好{Environment.NewLine}";
+            var sftp = Appliction.Extensions.Utils.Current.Resolve<ISftp>();
+
+            if (!FileExists())
+                return;
+        }
+
+        private bool FileExists()
+        {
+            var frontPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory ?? string.Empty, "Config", "front");
+            if (!Directory.Exists(frontPath))
+                Directory.CreateDirectory(frontPath);
+
+            //todo 判断dockerfile文件跟 nginx 配置文件是否存在
+            if (!File.Exists(Path.Combine(frontPath, "Dockerfile")))
+            {
+                Display.SelectedText += $"dockerfile文件不存在{Environment.NewLine}";
+                return false;
+            }
+
+            if (!File.Exists(Path.Combine(frontPath, "nginx.conf")))
+            {
+                Display.SelectedText += $"nginx.conf文件不存在{Environment.NewLine}";
+                return false;
+            }
+
+            return true;
         }
     }
 }
